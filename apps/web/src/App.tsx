69c14a5afd89df9b88145d90a05ba1954a2ee32c
Takato Nakamura <takato.nakamura@access-company.com>
2025-12-17 03:24:03 +0900

    add talks section with external and internal presentation details

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -5,10 +5,21 @@ function App() {
 
       <section>
         <h2>Talks</h2>
+        
+        <h3>外部登壇</h3>
         <ul>
           <li>
             <a href="https://atakatonakamura.github.io/TSKaigi_Hokuriku/1" target="_blank">
               2025/11/23 パイプ演算子をTypeScriptに勝手に実装して遊ぶ話 @ TSKaigi Hokuriku
+            </a>
+          </li>
+        </ul>
+
+        <h3>社内勉強会</h3>
+        <ul>
+          <li>
+            <a href="/slides/20251217" target="_blank">
+              2025/12/17 TSKaigi HokurikuでLT登壇してきた話
             </a>
           </li>
         </ul>
